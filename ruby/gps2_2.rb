f9beb408340f83957e67782845582a4d44d88525
Johnny Choo <jchoo156@gmail.com>
2016-07-27 19:52:05 -0400

    rename variables and add answers to questions

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -59,7 +59,7 @@ end
 
 def print_list(list)
 	list.each do |key, quantity|
-		puts "Item: #{key} Quantity: #{quantity} "
+		puts "#{key}: #{quantity}"
 	end
 end
 
@@ -73,16 +73,34 @@ update_quantity(list, "apples", 5)
 print_list(list)
 p list
 
-list2 = create_list("baseball bat hat")
-p list2 
+store_list = create_list("baseball bat hat")
+p store_list
 
-add_item(list2, "Lemonade", 2)
-add_item(list2, "Tomatoes", 3)
-add_item(list2, "Onions", 1)
-add_item(list2, "Ice Cream", 4)
+add_item(store_list, "Lemonade", 2)
+add_item(store_list, "Tomatoes", 3)
+add_item(store_list, "Onions", 1)
+add_item(store_list, "Ice Cream", 4)
 
-remove_item(list2, "Lemonade")
-update_quantity(list2, "Ice Cream", 1)
-print_list(list2)
+remove_item(store_list, "Lemonade")
+update_quantity(store_list, "Ice Cream", 1)
+print_list(store_list)
 
+# What did you learn about pseudocode from working on this challenge?
+#   It helps others to better understand your code and it has certain guidelines that should be followed.
+
+# What are the tradeoffs of using arrays and hashes for this challenge?
+#   For this challenge, it made more sense to use a hash because it's more readable to store things in a key and value sort of fashion.
+#   Arrays would have been better if it were just strictly items without quantities but that wasn't the case for this challenge.
+
+# What does a method return?
+#   The method returns whatever is on the last thing called is unless you do an early return or break.
+
+# What kind of things can you pass into methods as arguments?
+#   You can pass strings, arrays, integers as arguments to methods and some others.
+
+# How can you pass information between methods?
+#   You can pass information between methods by using methods within other methods.  You can also have a method take a method that is set into a variable and pass it in that way.
+
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+#   I have a better understanding of utilizing methods with other methods and how to pass information around.  I was mostly confused on how to best tackle this challenge and it was all explained along the way so I'm all set!
 
