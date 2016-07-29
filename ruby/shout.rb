bfb8ee831ec34f8dacf366f1e808cab3056ed7e1
Johnny Choo <jchoo156@gmail.com>
2016-07-28 21:37:31 -0400

    add two classes that include shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,48 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words + "!!!" + " :)"
+#   end
+# end
+
+# p Shout.yell_angrily("Darnit why did I do that")
+# p Shout.yelling_happily("I am so lucky")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    puts words + "!!!" + " >:("
   end
 
-  def self.yelling_happily(words)
-    words + "!!!" + " :)"
+  def yell_happily(words)
+    puts words + "!!!" + " ^_^"
   end
 end
 
-p Shout.yell_angrily("Darnit why did I do that")
-p Shout.yelling_happily("I am so lucky")
+class Adult
+  include Shout
+
+  def initialize
+    puts "---Adult---"
+  end
+end
+
+class Teenager
+  include Shout
+
+  def initialize
+    puts "---Teenager---"
+  end
+end
+
+adult = Adult.new
+adult.yell_angrily("Why didn't I file my taxes")
+adult.yell_happily("I got a raise")
+
+teenager = Teenager.new
+teenager.yell_angrily("Get out of my room")
+teenager.yell_happily("I can finally drive")
+
+
