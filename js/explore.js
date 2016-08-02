96ead79635e4df3c8d012609ffccec47f6720b57
Johnny Choo <jchoo@Johnnys-MacBook-Air.local>
2016-08-02 14:38:58 -0400

    add pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,13 +1,18 @@
 // FUNCTION that reverses a word by iterating through each 
 // letter from end to start in a VARIABLE.
 function reverse(word) {
+	// Create an empty STRING VARIABLE
 	var reversed_word = "";
+	// FOR statement creating a new VARIABLE i that equals the length of word
+	// from there, we start at the end of given word and iterate towards the start
+	// and store it in the reversed_word variable
 	for (var i = word.length - 1; i >= 0; i--) {
 		reversed_word+=word[i];
 	};
 	console.log(reversed_word);
 }
 
+// Driver code
 reverse("welcome")
 
 if (1 == 1) {
