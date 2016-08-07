4963a044ade5ca3b6868860ccc7b9ca47dad70a8
Johnny Choo <jchoo156@gmail.com>
2016-08-07 12:51:39 -0400

    fix algorithms and add proper pseudocode

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,9 @@
+// Create a FUNCTION that takes an ARRAY of words
+// Create an empty STRING VARIABLE named longest
+//   ITERATE through ARRAY
+//   IF current ARRAY item length is greater than the VARIABLE longest, set it equal to the current item.
+// Once ITERATION is complete, print out the VARIABLE longest
+
 // FUNCTION that takes an ARRAY as a PARAMETER and finds the longest STRING
 function longest_word(words) {
 	var longest = "";
@@ -10,15 +16,31 @@ function longest_word(words) {
 	console.log(longest)
 }
 
+
+// Create a FUNCTION that takes two HASH TABLE PARAMETERS
+// Iterate through HASH TABLES
+//  IF there are any key value matches, return true
+//  ELSE return false if no key value matches are found
+
 // FUNCTION that takes 2 OBJECTS and finds returns true if any key value matches occur
 function key_value_match(object1, object2) {
-	if (object1.name == object2.name || object1.age == object2.age) {
-		console.log(true);
+	var keys = Object.keys(object1);
+	var keys2 = Object.keys(object2);
+	for (var i = 0; keys.length > i; i++) {
+		if (object1[keys[i]] == object2[keys[i]]) {
+			return console.log(true)
+		}
 	}
-	else {
-		console.log(false);
-	}
+	console.log(false)
 }
+
+// Create a FUNCTION that takes a takes a INTEGER
+// Create VARIABLE and set it to equal the alphabet from a-z
+// Create a VARIABLE named result that will be used to store the final results
+//   Set a variable that will randomly generate a number from 1-10.  This will be used to determine the length of a word we create.
+//   ITERATE through the alphabet VARIABLE and grabs X amount of random letters, X being the randomly generated number.
+//   Once the random letters are grabbed, PUSH it into the result VARIABLE
+// Return the result
 
 // Takes an integer for length and builds and returns an array of strings of the given length
 function random_letters(number) {
@@ -47,6 +69,7 @@ console.log("-------------")
 key_value_match({name: "Steven", age: 54}, {name: "Tamir", age: 54})
 key_value_match({name: "Todd", age: 20}, {name: "Tam", age: 30})
 key_value_match({name: "Lemar", age: 33}, {name: "Lemar", age: 51})
+key_value_match({name: "Lemar", age: 2, race: "Asian"}, {name: "Lex", age: 51, gender: "Male"})
 
 console.log("-------------")
 
